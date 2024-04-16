96df2036bc5730a91d6f87f0ecf920b842080aa9
Natalie C Townley <nataliect98@gmail.com>
2024-04-16 15:07:53 -0500

    Created a function to determine user input and save it to a variable(userInput invalid currently)

--- index.js
+++ index.js
@@ -1,10 +1,31 @@
 import Express from "express";
 import bodyParser from "body-parser";
 import ejs from "ejs";
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 const app = Express();
 const port = 3000;
+var response = "";
+
+function userResponse (req, res, next) {
+    response = req.body["userInput"];
+    next;
+}
+
+app.use(userResponse);
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get ("/", (req, res) => {
+    res.sendFile(__dirname + "/index.html")
+});
 
 app.listen (port, (req, res) => {
 console.log(`Server is running on port number: ${port}`);
+});
+
+app.post("/submit", (req, res) => {
+    console.log(req.body);
+    res.send(`You chose to ${response}`);
 });
